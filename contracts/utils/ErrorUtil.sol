f68de516f8008fa3d403d455580e1b3bdc93ad89
joey <joey@cc.snxdao.io>
2023-11-09 15:11:16 +1100

    Add cancel order

--- contracts/utils/ErrorUtil.sol
+++ contracts/utils/ErrorUtil.sol
@@ -20,8 +20,14 @@ library ErrorUtil {
     // @notice Thrown when order not ready for settlement.
     error OrderNotReady();
 
+    // @notice Thrown when order trying owner trying to clear a stale order
+    error OrderNotStale();
+
     // @notice Thrown when an order cannot settle due to limitPrice tolerance not met.
     error PriceToleranceExceeded(int128 sizeDelta, uint256 price, uint256 limitPrice);
+
+    // @notice Thrown when an order cannot cancel due to limitPrice is met.
+    error PriceToleranceNotExceeded(int128 sizeDelta, uint256 price, uint256 limitPrice);
 
     // @notice Thrown during settlement when off-chain diverges too far from on-chain price.
     error PriceDivergenceExceeded(uint256 offchainPrice, uint256 onchainPrice);
@@ -76,4 +82,7 @@ library ErrorUtil {
 
     // @notice Thrown when the input arrays have mismatched lengths.
     error ArrayLengthMismatch();
+
+    // @notice Thrown when and action is only allowed by account owner
+    error OnlyAccountOwner();
 }
